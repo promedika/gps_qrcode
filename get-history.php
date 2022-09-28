706868305f364ec41c34ba534bb3d36ed0bd8693
andrian.dwiari <andrian@globalpromedika.co.id>
2022-09-28 11:36:36 +0700

    update koneksi di get history

--- get-history.php
+++ get-history.php
@@ -1,14 +1,6 @@
 <?php
-    $servername = "localhost:3307";
-    $username = "root";
-    $password = "";
-    $dbname = "list_rs_qrcode";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        echo "Connection failed: ".$conn->connect_error;
-        die();
-    }
+    //Include file koneksi ke database
+    include "koneksi.php";
 
     // Query history rs
     $sql = "SELECT * FROM tbl_history LEFT JOIN tbl_rs ON th_tbl_rs_tr_id = tr_id ORDER BY th_id DESC LIMIT 1";
